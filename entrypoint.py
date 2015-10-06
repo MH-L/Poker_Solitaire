f128ad238557a42f35ee31166646d47f24f107af
MH-L <922lmh922@gmail.com>
2015-10-06 14:41:23 +0800

    Edited main function.

--- entrypoint.py
+++ entrypoint.py
@@ -1,7 +1,21 @@
 __author__ = 'Minghao'
+INVALID_CHOICE_CONSTANT = 0
 
 def main():
-    pass
+    print "The Poker Game is starting."
+    print "Please enter 1 for normal game, 2 for hearts game,\n" \
+          "and 3 for UPLevel game."
+    choice = raw_input("Please enter your choice.")
+    try:
+        choice = int(choice)
+    except ValueError:
+        choice = INVALID_CHOICE_CONSTANT
+    while choice not in [1,2,3]:
+        choice = raw_input("Invalid option. Please enter again.")
+        try:
+            choice = int(choice)
+        except ValueError:
+            choice = INVALID_CHOICE_CONSTANT
 
 if __name__ == "__main__":
     main()
