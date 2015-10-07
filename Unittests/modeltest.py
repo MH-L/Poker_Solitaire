884dad90e489590ad74ffb2767a1435a7cc4e872
MH-L <922lmh922@gmail.com>
2015-10-07 10:56:24 +0800

    Added tests, although they are failing.

--- Unittests/modeltest.py
+++ Unittests/modeltest.py
@@ -1,5 +1,28 @@
 __author__ = 'Minghao'
 from unittest import TestCase
+from model import (
+    Poker, Game, Player, Deck
+)
+from constants import (
+    RESULT_LARGER, RESULT_SMALLER, RESULT_INDIFFERENT
+)
+
 
 class PokerTestCase(TestCase):
-    pass
+    poker1 = Poker(1,0)
+    poker2 = Poker(2,0)
+    poker3 = Poker(4,1)
+    poker4 = Poker(9,4)
+    poker5 = Poker(12,1)
+
+    def testCompareCards(self):
+        poker1 = Poker(1,0)
+        poker2 = Poker(2,0)
+        poker3 = Poker(4,1)
+        poker4 = Poker(9,4)
+        poker5 = Poker(12,1)
+        self.assertEquals(poker1.compare(poker2), RESULT_LARGER)
+        self.assertEquals(poker2.compare(poker5), RESULT_LARGER)
+        self.assertEquals(poker5.compare(poker4), RESULT_LARGER)
+        self.assertEquals(poker3.compare(poker5), RESULT_SMALLER)
+        pass
