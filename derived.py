7a48701c523811ed9dd4a9ec6497ba558c85c939
MH-L <922lmh922@gmail.com>
2015-10-10 15:56:43 +0800

    Added prompting user input of cards to pullout.

--- derived.py
+++ derived.py
@@ -5,7 +5,7 @@ __author__ = 'Minghao'
 
 class NormalGame(Game):
     def __init__(self):
-        super(self, NormalGame).__init__()
+        super(NormalGame, self).__init__()
         # do something here.
 
     def compare_set(self, set1, set2):
@@ -43,4 +43,47 @@ class HeartGameComPlayer(ComputerPlayer):
 
 
 class UPLevelGameComPlayer(ComputerPlayer):
+    pass
+
+
+class HumanPlayer(Player):
+    def make_turn(self, current_set):
+        isValidChoice = False
+        while not isValidChoice:
+            indices = list()
+            hasInvalid = False
+            userInput = raw_input("Enter a sequence of cards, separated by commas.")
+            cards = userInput.split(",")
+            for card in cards:
+                try:
+                    index = int(card)
+                except ValueError:
+                    hasInvalid = True
+            if hasInvalid:
+                print "The input you entered is not valid."
+            else:
+                isValidChoice = True
+
+        isOkayChoice = self.validate_choice(indices, current_set)
+
+    def validate_choice(self, choices, current_set):
+        return False
+    pass
+
+
+class NormalHumanPlayer(HumanPlayer):
+    def validate_choice(self, choices, current_set):
+        pass
+    pass
+
+
+class HeartsHumanPlayer(HumanPlayer):
+    def validate_choice(self, choices, current_set):
+        pass
+    pass
+
+
+class UPLevelHumanPlayer(HumanPlayer):
+    def validate_choice(self, choices, current_set):
+        pass
     pass
